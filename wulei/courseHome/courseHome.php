ba2ba09fe0653954eba7d93bb52d6aeeda311d2c
lanou <lan@lanoudeMac-mini-111.local>
2016-07-28 20:16:30 +0800

    7-28 wulei 8:16

---
+++ wulei/courseHome/courseHome.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>线下课程</title>
+	<link rel="stylesheet" type="text/css" href="../../css/course/courseHome.css">
+	<link rel="stylesheet" type="text/css" href="../../css/base.css">
+	<link rel="stylesheet" type="text/css" href="../../css/head_footer.css">
+	<link rel="stylesheet" type="text/css" href="../../css/iconfont.css">
+</head>
+<body>
+<!--***********************header开始**************************-->
+<header>
+	<div class="limit">
+		<a class="jyLogo" href="../../index.html"></a>
+		<ul class="navigation">
+			<li><a class="active" href="javascript:void(0);">课程</a></li>
+			<li><a href="javascript:void(0);">学习指导</a></li>
+			<li><a href="javascript:void(0);">企业招聘</a></li>
+			<li><a href="javascript:void(0);">互联网头条</a></li>
+			<li><a href="javascript:void(0);"><i class="iconfont cross">&#xe665;</i>提问</a></li>
+		</ul>
+		<a href="javascript:void(0);" class="personalBtn">
+			<div class="headPic">
+				<img src="../../img/head_footer/head_footer_03.png"/>
+			</div>
+			<span>个人中心</span>
+			<i class="iconfont down">&#xe818;</i>
+		</a>
+	</div>
+</header>
+<!--***********************header结束**************************-->
+
+<!--***********************导航窗开始**************************-->
+<nav class="course-classfy">
+<div class="course-content-classfy">
+<span class="classfy-icon"></span>
+<span class="classfy-word">分类:</span>
+<ul class="classfy-content-list clearfix">
+	<li><a href="javascript:void(0)">全部</a></li>
+	<li><a href="javascript:void(0)">unity 3D</a></li>
+	<li><a href="javascript:void(0)">iOS</a></li>
+	<li><a href="javascript:void(0)">Android</a></li>
+	<li><a href="javascript:void(0)">Html5</a></li>
+</ul>
+	
+</div>
+<div class="course-location-classfy">
+<span class="location-icon"></span>
+<span class="classfy-word">区域:</span>
+<ul class="classfy-content-list clearfix">
+	<li><a href="javascript:void(0)">全部</a></li>
+	<li><a href="javascript:void(0)">朝阳区</a></li>
+	<li><a href="javascript:void(0)">海淀区</a></li>
+	<li><a href="javascript:void(0)">西城区</a></li>
+	<li><a href="javascript:void(0)">东城区</a></li>
+</ul>	
+</div>
+</nav>
+<!--***********************导航窗结束**************************-->
+
+<!--***********************内容开始**************************-->
+<section class="course-content">
+<nav>
+	<ul class="course-reorder clearfix">
+		<li class="course-reorder-active"><a href="javascript:void(0)" class="default">默认</a></li>
+		<li><a href="javascript:void(0)" class="sale-volume">销量</a></li>
+		<li><a href="javascript:void(0)" class="sale-price">价格</a></li>
+		<li><a href="javascript:void(0)" class="high-opinion">好评</a></li>
+	</ul>
+</nav>
+	<ul class="course-lists clearfix">
+<!-- 		<li class="course-brief">
+		<a href="javascript:void">
+		<div class="img-box">
+		<img src="../../img/course/example.png">
+		<span id="course-count">7.6分</span>				
+		</div>
+		<p class="course-name">英语零基础直达四级水平新概念</p>
+		<p class="course-adress">[南京市]新街口中山南路8号苏豪大厦</p>
+		<div class="price-details clearfix">
+				<span class="course-price1">￥</span>
+				<span class="course-price2">1000</span>
+				
+					<span class="ori-price1">￥</span>
+					<del class="ori-price">100</del>
+				<span class="sold-num">已售129033</span>	
+		</div>
+		</a>
+		</li> -->
+
+	</ul>
+
+<nav class="page-shift">
+	<a href="javascript:void(0)"><span class="pre-page ">上一页</span></a>
+<!-- 	<a href="javascript:void(0)"><span class="page-active">1</span></a>
+	<a href="javascript:void(0)"><span>2</span></a>
+	<a href="javascript:void(0)"><span>3</span></a>
+	<a href="javascript:void(0)"><span>4</span></a>
+	<a href="javascript:void(0)"><span>5</span></a>
+	<a href="javascript:void(0)"><span>6</span></a>
+	<a href="javascript:void(0)"><span>7</span></a>
+	<a href="javascript:void(0)"><span>8</span></a>
+	<a href="javascript:void(0)"><span>9</span></a>
+	<a href="javascript:void(0)"><span>...</span></a>
+	<a href="javascript:void(0)"><span>100</span></a> -->
+	<a href="javascript:void(0)"><span class="next-page ">下一页</span></a>
+</nav>
+</section>	
+<!--***********************内容结束**************************-->
+<footer>
+			<div class="limit">
+				<p class="friendlyLink">
+					<span>友情链接</span>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a href="javascript:void(0);">蓝鸥科技</a>
+					<a class="more" href="javascript:void(0);">更多<i class="iconfont">&#xe6ac;</i></a>
+					
+				</p>
+				<div class="comprehensiveInformation">
+					<ul>
+						<li>
+							<p>我们的实验室</p>
+						</li>
+						<li>
+							<a href="javascript:void(0);">在线编程</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">奖学制度</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">在线班级</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">课后60分钟考试</a>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<p>关于我们</p>
+						</li>
+						<li>
+							<a href="javascript:void(0);">精英吧简介</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">联系我们</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">加入我们</a>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<p>现有课程</p>
+						</li>
+						<li>
+							<a href="javascript:void(0);">iOS</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">Android</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">unity3D</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">H5</a>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<p>帮助</p>
+						</li>
+						<li>
+							<a href="javascript:void(0);">联系客服</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">版权声明</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">法律声明</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">服务条款</a>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<p>反馈方式</p>
+						</li>
+						<li>
+							<a href="javascript:void(0);">电子邮箱：help@xxxxx.com</a>
+						</li>
+						<li>
+							<a href="javascript:void(0);">微信反馈：精英吧微信公众号</a>
+						</li>
+						<li>
+							<img src="../../img/head_footer/head_footer_10.png"/>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<p>精英吧APP扫描下载</p>
+						</li>
+						<li>
+							<img src="../../img/head_footer/head_footer_07.png"/>
+						</li>
+					</ul>
+				</div>
+				<div class="license">
+					<p>蓝鸥科技有限公司©1997-2016 <span>|</span> 北京市海淀区清河毛纺路路南甲36号金五星商业大厦5层</p>
+				</div>
+			</div>
+		</footer>
+<script type="text/javascript" src="../../js/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() {
+$(".course-reorder li").on("click",function(){
+	var index = $(this).index();
+	$(this).addClass("course-reorder-active").siblings().removeClass("course-reorder-active");
+	$(".course-reorder a").css({
+		color:"#333"
+	});
+	$(".course-reorder a").eq(index).css({
+		color:"#29b572"
+	})
+})
+
+		function createPage() {
+	
+			$.ajax({
+				type:"GET",
+				url:"../../php/couseHome2.php",//接口地址
+				dataType:"json",
+				success:function(data) {
+                var course_leg = data.data.length;
+                course_leg = Math.ceil(course_leg/15);
+               
+                for(var i = 1; i <= course_leg; i++){
+                	var $a = $("<a></a>");
+                	if(i == 1){
+                	var content = '<span class="page-num page-num-active page-num' + i + '">' + i + '</span>'	
+                	}
+                	else{
+                	 var content = '<span class="page-num page-num' + i + '">' + i + '</span>'	
+                	}
+				   
+				    $a.attr({  href: "javascript:void(0);" });
+					$a.html(content);
+					$a.insertBefore($(".next-page"));
+
+                }
+                
+					
+				},
+				error:function() {
+					console.log("请求失败");
+				}
+			})
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		    var page = 0;
+		    var flag = true;
+		function pushData() {
+			flag = false;
+			$.ajax({
+				type:"GET",
+				url:"../../php/couseHome1.php",//接口地址
+				dataType:"json",
+				data:{
+					page:page
+				},
+				success:function(data) {
+				if(data.data.length > 0) {
+					    flag = true;
+						imgLoad(data);
+						console.log("aj-1 " + page)
+						page++;
+						console.log("aj " + page)
+							if(data.data.length != 15) {
+							//10是每次请求数据库返回的数据条数
+							flag = false;
+						}
+			    }
+					
+				},
+				error:function() {
+					console.log("请求失败");
+				}
+			})
+		}
+
+		function waterfall(data) {
+			$.each(data.data,function(index,key) {
+				
+
+				//dom操作
+				var class_price = key.classPrice.substring(0,key.classPrice.length - 1);
+				var class_name = key.classType;
+				if(class_name.length > 16){
+					class_name = class_name.substring(0,13);
+					class_name += "..." 
+
+				}
+				var $div = $("<div></div>");
+				var content = 		'<li class="course-brief">' +
+									'<a href="../courseDetails/otherCourse.php?value=' + index + '">' +
+									'<div class="img-box">' +
+									'<img src="' + key.imgAddress + '">' +
+									'<span id="course-count">7.6分</span>' + 			
+									 '</div>' + 
+									'<p class="course-name">' + class_name + '</p>' +
+									'<p class="course-adress">' + key.shoolAddress + '</p>' +
+									'<div class="price-details clearfix">' + 
+											'<span class="course-price1">￥</span>' + 
+											'<span class="course-price2">' + class_price + '</span>' + 
+											
+												'<span class="ori-price1">￥</span>' + 
+												'<del class="ori-price">100</del>' +
+											'<span class="sold-num">已售129033</span>' +
+									'</div>' +
+									'</a>' +
+									'</li>'
+					$div.html(content);
+					$(".course-lists").append($div);
+				})
+			}
+		function imgLoad(data) {
+			//图片预加载
+			var imgLen = 0;
+			for(var i = 0,len = data.data.length; i < len ; i++) {
+				var img = new Image();
+				img.src = data.data[i].imgAddress;
+				img.onload = function() {
+					imgLen ++;
+					if(imgLen == len) {
+						//所有的图片都加载好了
+						waterfall(data);
+					}
+				}
+			}
+		}
+
+		pushData();
+		console.log(" puhdate()" + page);
+
+		createPage();
+		console.log(" createPage()" + page);
+
+       $(".next-page").on("click",function(){
+       if(flag) {
+       	        $(".course-lists").html(" ");
+       	         console.log(" next-page-1" + page);
+				pushData();
+					 $(".page-num").removeClass("page-num-active");
+	       			 $(".page-num").eq(page).addClass("page-num-active");
+	       			 console.log(" next-page" + page);
+			}
+
+       })
+       $(".pre-page").on("click",function(){
+       	if(page > 1){
+       		console.log("pre-pageclick" + page);
+       		page = page - 2;
+       		
+   		    $(".course-lists").html(" ");
+			pushData();
+				     $(".page-num").removeClass("page-num-active");
+	       			 $(".page-num").eq(page).addClass("page-num-active");
+       	}
+      })
+       $(".page-shift").on("click",function(e){
+       		var e = e || e.event;
+       		var target = e.target || e.srcElement;
+
+       		if(target.className.indexOf("page-num") > -1){
+
+       			var index = target.className;
+       			index = index.substring(index.length-1,index.length)
+       			if(!(page == index)){
+       			page = index-1;
+	       			$(".course-lists").html(" ");
+	       			pushData();
+	       			 $(".page-num").removeClass("page-num-active");
+	       			 $(".page-num").eq(index-1).addClass("page-num-active");
+
+       			}
+
+
+
+       		}
+
+       })
+
+
+
+})
+
+
+
+
+</script>
+</body>
+</html>
